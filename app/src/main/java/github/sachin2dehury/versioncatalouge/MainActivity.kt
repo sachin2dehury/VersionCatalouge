5be1ea3b2a8187001644cc7a0d28a1d9bb490566
Sachin Dehury <51115041+sachin2dehury@users.noreply.github.com>
2024-01-22 11:01:41 +0530

    init

---
+++ app/src/main/java/github/sachin2dehury/versioncatalouge/MainActivity.kt
@@ -0,0 +1,46 @@
+package github.sachin2dehury.versioncatalouge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import github.sachin2dehury.versioncatalouge.ui.theme.VersionCatalougeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VersionCatalougeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    VersionCatalougeTheme {
+        Greeting("Android")
+    }
+}
